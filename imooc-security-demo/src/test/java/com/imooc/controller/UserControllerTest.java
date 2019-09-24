987d42aad476d90f723132da203dde9c725c668d
kevin <kevin_wangqi@126.com>
2019-09-24 23:06:37 +0800

    上传文件

--- imooc-security-demo/src/test/java/com/imooc/controller/UserControllerTest.java
+++ imooc-security-demo/src/test/java/com/imooc/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
 /**
@@ -99,5 +101,14 @@ public class UserControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.delete("/user/1")
                 .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void whenUploadSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/file")
+                .file(new MockMultipartFile("file", "test.txt",
+                        "multipart/form-data", "hello upload".getBytes("UTF-8"))))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
     }
 }
