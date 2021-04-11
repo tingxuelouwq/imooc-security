9745dad0b7aed5770b790615993363db5f47af3e
kevin <kevin_wangqi@126.com>
2021-04-11 19:51:52 +0800

    rbac

--- imooc-security-demo/src/main/java/com/imooc/security/DemoAuthorizeConfigProvider.java
+++ imooc-security-demo/src/main/java/com/imooc/security/DemoAuthorizeConfigProvider.java
@@ -1,19 +1,17 @@
 package com.imooc.security;
 
-import org.springframework.core.annotation.Order;
+import com.imooc.security.core.authorize.AuthorizeConfigProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.stereotype.Component;
 
-import com.imooc.security.core.authorize.AuthorizeConfigProvider;
-
 @Component
-@Order(Integer.MAX_VALUE)
 public class DemoAuthorizeConfigProvider implements AuthorizeConfigProvider {
 
 	@Override
 	public void config(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry config) {
-		config.anyRequest().access("@rbacService.hasPerssion(request, authentication)");
+		config.anyRequest().access("@rbacService.hasPermission(request, authentication)");
+
 	}
 
 }
